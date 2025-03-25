06d887237b0c10519ab26fcae3f6a673eea03064
JongCo <xn1128@gmail.com>
2025-03-25 22:14:45 +0900

    술식 반전 '아카' (픽셀 출력 방향을 반대로 간단하게 만듬)

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,11 @@ uint32_t colors[60];
 
 RTCModeManager rtcModeManager;
 
+bool isReverseMode = true;
+
 void pushLeft(uint32_t *brights);
 void setRandomColorArray();
+void nextPixel(uint8_t* indexRef, bool isReverseMode);
 
 void setup() {
   neoPixel.begin();
@@ -31,7 +34,12 @@ void setup() {
 void loop() {
   neoPixel.clear();
 
-  uint8_t pixelPlace = 0;
+  uint8_t pixelPlace;
+  if (isReverseMode) {
+    pixelPlace = NUMPIXELS-1;
+  } else {
+    pixelPlace = 0;
+  }
 
   Array<uint32_t, RTC_HOUR_PIXELNUM> hourPattern
     = rtcModeManager.getHoursToColorArray(
@@ -55,43 +63,43 @@ void loop() {
   
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
 
   // hours
   for (size_t i = 0; i < RTC_HOUR_PIXELNUM; i++) {
     neoPixel.setPixelColor(pixelPlace, hourPattern[i]);
-    pixelPlace++;
+    nextPixel(&pixelPlace, isReverseMode);
   }
 
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
 
   // minutes
   for (size_t i = 0; i < RTC_MIN_PIXELNUM; i++) {
     neoPixel.setPixelColor(pixelPlace, minutePattern[i]);
-    pixelPlace++;
+    nextPixel(&pixelPlace, isReverseMode);
   }
 
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
 
   // seconds
   for (size_t i = 0; i < RTC_SEC_PIXELNUM; i++) {
     neoPixel.setPixelColor(pixelPlace, secondsPattern[i]);
-    pixelPlace++;
+    nextPixel(&pixelPlace, isReverseMode);
   }
 
   // divider
   neoPixel.setPixelColor(pixelPlace, Adafruit_NeoPixel::ColorHSV(16000, 255, 50));
-  pixelPlace++;
+  nextPixel(&pixelPlace, isReverseMode);
 
   neoPixel.show();
 
@@ -111,5 +119,13 @@ void pushLeft(uint32_t* target) {
 void setRandomColorArray() {
   for (int i = 0; i < NUMPIXELS; i++) {
     colors[i] = Adafruit_NeoPixel::ColorHSV(random(1000, 8000), 255, 255);
+  }
+}
+
+void nextPixel(uint8_t* indexRef, bool isReverseMode) {
+  if (isReverseMode) {
+    *indexRef = *indexRef-1;
+  } else {
+    *indexRef = *indexRef+1;
   }
 }
